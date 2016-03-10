838e07f9ca3c11dccb7ab771a880a68cb1337e70
崇大 <ym.sunshine.moon.15963@gmail.com>
2016-03-10 12:17:52 +0900

    first commit

---
+++ test16.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<header>
+	<style type="text/css">
+	#header {
+		background-color: #7AAE2A;
+		width: 100%;
+		margin: auto;
+		margin-bottom: 20px;
+		margin-top: 0;
+		padding: 10px;
+		box-shadow: 0 4px 2px -2px gray;
+	}
+	</style>
+	</header>
+	<body>
+	<div id="header">
+		<img id="logo" src=logo.png>
+	</div>
+	</body>
+</html>
